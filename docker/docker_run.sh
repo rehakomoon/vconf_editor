94a33a463713ed5a049b3baecf4984f077044113
rehakomoon <re.hako.moon@gmail.com>
2024-02-02 16:17:15 +0900

    pilot impl of text replacements

--- docker/docker_run.sh
+++ docker/docker_run.sh
@@ -1,6 +1,7 @@
 docker run --rm -it -v $PWD/template:/template -v $PWD/output:/output vconf_editor sh -c ' \
 cp -r /template/* /workdir && \
 cd /workdir && \
+python insert_texts.py && \
 latexmk -C main.tex && \
 latexmk main.tex && \
 latexmk -c main.tex && \
