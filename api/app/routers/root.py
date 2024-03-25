f779d2fc5b6571d5e9bc3b657f7f8472ba686cdb
sansuke05 <takanori5064@gmail.com>
2024-03-26 04:33:35 +0900

    rootのエンドポイントを別モジュールに切り出し

---
+++ api/app/routers/root.py
@@ -0,0 +1,13 @@
+from fastapi import APIRouter
+from fastapi.responses import FileResponse
+
+router = APIRouter()
+
+@router.get("/")
+async def root():
+    return {"message": "Hello World"}
+
+
+@router.get("/testform")
+async def testform():
+    return FileResponse("./app/testform.html")
