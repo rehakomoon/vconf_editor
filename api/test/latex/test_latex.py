13cf73b36d2700625ab33df4d68315b5c71ee30b
cleantted <seo.cleantted@gmail.com>
2024-04-18 00:37:31 +0900

    ✨ add replacing title test

---
+++ api/test/latex/test_latex.py
@@ -0,0 +1,20 @@
+from app.latex import latex
+from app.schemas.paper import Paper
+
+
+def test_create_latex_text_titleが置き換えられる():
+    template = "title:<<<title>>>\nother:<<<other>>>"
+
+    paper = Paper(
+        title="タイトル",
+        author="著者",
+        abstract="アブストの文章",
+        body=[],
+        teaser=None,
+        figure=[],
+    )
+
+    expect = "title:タイトル\nother:<<<other>>>"
+
+    actual = latex.create_latex_text(template, paper, False)
+    assert actual == expect
