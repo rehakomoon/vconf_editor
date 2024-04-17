81a51d7eb250a5caf20d4f66b9eeaf026bef56c1
cleantted <seo.cleantted@gmail.com>
2024-04-18 00:44:10 +0900

    ✨ add easy test case for create_latex_text

--- api/test/latex/test_latex.py
+++ api/test/latex/test_latex.py
@@ -18,3 +18,39 @@ def test_create_latex_text_titleが置き換えられる():
 
     actual = latex.create_latex_text(template, paper, False)
     assert actual == expect
+
+
+def test_create_latex_text_authorが置き換えられる():
+    template = "author:<<<author>>>\nother:<<<other>>>"
+
+    paper = Paper(
+        title="タイトル",
+        author="著者",
+        abstract="アブストの文章",
+        body=[],
+        teaser=None,
+        figure=[],
+    )
+
+    expect = "author:著者\nother:<<<other>>>"
+
+    actual = latex.create_latex_text(template, paper, False)
+    assert actual == expect
+
+
+def test_create_latex_text_abstractが置き換えられる():
+    template = "abstract:<<<abstract>>>\nother:<<<other>>>"
+
+    paper = Paper(
+        title="タイトル",
+        author="著者",
+        abstract="アブストの文章",
+        body=[],
+        teaser=None,
+        figure=[],
+    )
+
+    expect = "abstract:アブストの文章\nother:<<<other>>>"
+
+    actual = latex.create_latex_text(template, paper, False)
+    assert actual == expect
