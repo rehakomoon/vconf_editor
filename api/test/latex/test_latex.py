dedb58e4a8f7f61b7c4617eec3a58d831ac0b200
cleantted <seo.cleantted@gmail.com>
2024-08-04 19:07:56 +0900

    🔧 add test for replacing reference

--- api/test/latex/test_latex.py
+++ api/test/latex/test_latex.py
@@ -1,5 +1,5 @@
 from app.latex import latex
-from app.schemas.paper import Paper
+from app.schemas.paper import Paper, Reference
 
 
 def test_create_latex_text_titleが置き換えられる():
@@ -12,6 +12,7 @@ def test_create_latex_text_titleが置き換えられる():
         body=[],
         teaser=None,
         figure=[],
+        reference=[],
     )
 
     expect = "title:タイトル\nother:<<<other>>>"
@@ -30,6 +31,7 @@ def test_create_latex_text_authorが置き換えられる():
         body=[],
         teaser=None,
         figure=[],
+        reference=[],
     )
 
     expect = "author:著者\nother:<<<other>>>"
@@ -48,9 +50,29 @@ def test_create_latex_text_abstractが置き換えられる():
         body=[],
         teaser=None,
         figure=[],
+        reference=[],
     )
 
     expect = "abstract:アブストの文章\nother:<<<other>>>"
+
+    actual = latex.create_latex_text(template, paper, False)
+    assert actual == expect
+
+
+def test_create_latex_text_referenceが置き換えられる():
+    template = "reference:<<<reference>>>\nother:<<<other>>>"
+
+    paper = Paper(
+        title="タイトル",
+        author="著者",
+        abstract="アブストの文章",
+        body=[],
+        teaser=None,
+        figure=[],
+        reference=[Reference(value="value1"), Reference(value="value2")],
+    )
+
+    expect = "reference:\\bibitem{ref0} value1\n\\bibitem{ref1} value2\n\nother:<<<other>>>"
 
     actual = latex.create_latex_text(template, paper, False)
     assert actual == expect
