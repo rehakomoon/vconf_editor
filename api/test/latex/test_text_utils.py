535734011e766bf6adfa5bb7ce4df132773c0060
sansuke05 <takanori5064@gmail.com>
2024-08-24 03:33:05 +0900

    update test text

--- api/test/latex/test_text_utils.py
+++ api/test/latex/test_text_utils.py
@@ -1,9 +1,9 @@
 from app.latex import text_utils
 
 def test_escape_underscore():
-    test_text = "Hello_world! This_is_a_sample_text."
+    test_text = "Hello_world! This__is_a_sample_text."
 
-    expect = "Hello\_world! This\_is\_a\_sample\_text."
+    expect = "Hello\_world! This\_\_is\_a\_sample\_text."
 
     actual = text_utils.escape_underscore(test_text)
     assert actual == expect
