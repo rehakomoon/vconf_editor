1fedf98a81cc986581947543a2eead46fd610d76
sansuke05 <takanori5064@gmail.com>
2024-10-05 01:04:30 +0900

    特殊文字のエスケープを実装

--- api/app/latex/text_utils.py
+++ api/app/latex/text_utils.py
@@ -1,2 +1,22 @@
+import re
+
+
 def escape_spacial_characters(text: str) -> str:
-    return ""
+    escape_map = {
+        '#': r'\#',
+        '$': r'\$',
+        '%': r'\%',
+        '&': r'\&',
+        '~': r'\textasciitilde ',
+        '_': r'\_',
+        '^': r'\textasciicircum ',
+        '\\': r'\textbackslash ',
+        '{': r'\{',
+        '}': r'\}',
+    }
+
+    # 特殊文字にマッチする正規表現パターン
+    pattern = r'([#$%&~_^\\{}])'
+
+    # マッチした文字を対応するエスケープシーケンスに置換
+    return re.sub(pattern, lambda match: escape_map[match.group()], text)
